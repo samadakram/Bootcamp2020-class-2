2dbcea2e883cb30e10bb7aade509e83f42e7ab69
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-03 17:14:14 +0500

    Update App.js
    Added my Name with my Profession and Subjects in which I'm expert.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello World By PanaCloud!</h1>
+        Hello <strong> Abdul Samad!</strong>
+        <h6>Frontend Developer</h6>
+        <p>Expert in:</p>
+        <ol>
+          <li>HTML5</li>
+          <li>CSS3
+            <ul>
+              <li>Bootstrap</li>
+            </ul>
+          </li>
+          <li>JavaScript
+            <ul>
+              <li>ReactJs</li>
+              <li>Redux</li>
+              <li>React Redux</li>
+            </ul>
+          </li>
+        </ol>
     </div>
   );
 }
